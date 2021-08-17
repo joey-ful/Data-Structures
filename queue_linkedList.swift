a3f3e562086aaeac0312524dd3dcc282402875c2
joey-ful <isjoeyful@gmail.com>
2021-08-17 18:51:49 +0900

    queue using linked list

---
+++ queue_linkedList.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct Queue<Element> {
+    private var linkedList = LinkedList<Element>()
+    
+    var peek: Element? {
+        linkedList.peek?.data
+    }
+    
+    var isEmpty: Bool {
+        linkedList.isEmpty
+    }
+    
+    mutating func enqueue(_ newElement: Element) {
+        linkedList.append(data: newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        linkedList.pop()?.data
+    }
+    
+    mutating func clear() {
+        linkedList.clear()
+    }
+}
